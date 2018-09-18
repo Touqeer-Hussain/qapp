b804a7130b5b52893195bec900dd2a9b294bca35
Touqeer Hussain <touqeerhussain@zoho.com>
2018-09-18 19:53:45 +0500

    init

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import Container from './container/container'
+
+
+
+
+class App extends Component {
+constructor(){
+  super();
+  
+  this.state = {
+    
+  }
+}  
+
+  
+  
+  
+  render() {
+      return(
+        <div className="App">
+        
+        </div>
+      );
+  }
+}
+
+export default App;
